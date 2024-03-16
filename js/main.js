08f7374249fb2b7523541e84699f1cfc5c34517a
moaz ibrahem <moazibrahem952@gmail.com>
2024-03-16 20:58:43 +0200

    apple

--- js/main.js
+++ js/main.js
@@ -8,14 +8,24 @@ let usephone = document.getElementById("usephone");
 
 window.onscroll = function () {
     let value = scrollY ;
-    if (scrollY >= 5000) {
-        airbods.style.opacity = value/800 + "0" ;
-    } else {
+    if (scrollY >= 500) {
+        airbods.style.visibility =  "visible" ;
+        if (airbods.style.visibility =  "visible"){
+            airbods.style.opacity = value/800 + "0" ;
+        }
+    }else {
         airbods.style.opacity = value/800 - "0" ;
     }
-    if (scrollY >= 844) {
-        backphone.style.opacity = value/800 + "0" ;
-        caverphone.style.opacity = value/800 + "0" ;
-        conect.style.opacity = value/800 + "0" ;
+    if (scrollY >= 750) {
+        conect.style.visibility =  "visible" ;
+        if (conect.style.visibility =  "visible"){
+            conect.style.opacity = value/1500 + "0" ;
+        }
+        // backphone.style.opacity = value/800 + "0" ;
+        // caverphone.style.opacity = value/800 + "0" ;
+    }else {
+        conect.style.visibility =  "hidden" ;
+        conect.style.opacity = value/1500 - "0" ;
     }
-}
+    console.log(value);
+};
