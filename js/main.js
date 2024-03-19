69b0a127eda4940d7156885df168604bf3bd777f
moaz ibrahem <moazibrahem952@gmail.com>
2024-03-19 02:35:39 +0200

    apple3

--- js/main.js
+++ js/main.js
@@ -6,16 +6,25 @@ let caverphone = document.getElementById("caverphone");
 let usephone = document.getElementById("usephone");
 let useingPargraph = document.getElementById("useing_pargraph");
 
-
+// control scroll padge 
 window.onscroll = function () {
     let value = scrollY ;
-    if (scrollY >= 520) {
+
+    // control airbods pro with scroll  
+    if (scrollY >= 490) {
         airbods.style.visibility =  "visible" ;
         airbods.style.opacity = value/600 + "0" ;
+        airbods.style.top = 78 + "%" ;
+        airbods.style.width = 270 + "px" ;
     }else {
-        airbods.style.opacity = value/800 - "0" ;
+        airbods.style.visibility =  "hidden" ;
+        airbods.style.opacity = value/600 - "0" ;
+        airbods.style.top = 70 + "%" ;
+        airbods.style.width = 300 + "px" ;
     }
-    if (scrollY >= 600) {
+
+    // control img content pro with scroll  
+    if (scrollY >= 561) {
         conect.style.visibility =  "visible" ;
         contentPargraph.style.visibility =  "visible" ;
         contentPargraph.style.top =  60 + "%" ;
@@ -29,6 +38,8 @@ window.onscroll = function () {
         contentPargraph.style.opacity = value/800 - "0" ;
         contentPargraph.style.top =  60 + "%" ;
     }
+
+        // control img use airbods pro with scroll  
     if (scrollY >= 800){
         conect.style.display = "none";
         contentPargraph.style.top =  20 + "%" ;
@@ -55,5 +66,7 @@ window.onscroll = function () {
         useingPargraph.style.opacity = value/1200 - "0" ;
         useingPargraph.style.top =  80 + "%" ;
     }
+    
+    // view scroll padge 
     console.log(value);
 };
